f07404abe270109749d8de33a383f06b2586cfae
emrujuanCUH <meepwnd.fg@gmail.com>
2025-07-05 00:04:41 +0800

    routing fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { HashRouter } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <App />
+  <HashRouter>
+    <App />
+  </HashRouter>
 );
